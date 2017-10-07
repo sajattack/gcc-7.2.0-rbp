f0a17ee5bccb2e8279d0cd4bde05cfbd3db6481f
Paul Sajna <paulsajna@gmail.com>
2017-10-07 09:50:01 -0700

    initial commit

---
+++ lib/gcc/arm-linux-gnueabihf/7.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-7.2.0/configure --prefix=/opt/gcc-7.2.0-rbp/ --target=arm-linux-gnueabihf --enable-languages=c,c++ --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "tls", "gnu" } };
